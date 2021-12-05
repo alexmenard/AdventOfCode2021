1f5f864df3b993126d0974d7e185eaadf7757229
Alexandre Menard <acuz123@hotmail.fr>
2021-12-05 00:23:01 -0500

    commit part 2

--- day1/measurements.php
+++ day1/measurements.php
@@ -3,9 +3,12 @@
 #Advent Of Code 2021 - Day #1 - Part #1
 #Source : https://adventofcode.com/2021/day/1
 
-$file = './inputs/input_part1.txt';
-$measurements = 0;
-$last_depth = 0;
+$file            = '../inputs/input_day1.txt';
+$measurements    = 0;
+$measurements_p2 = 0;
+$last_depth      = 0;
+$sums            = array();
+$last_sum        = 0;
 
 $input  = fopen($file, "r") or die("Error opening file");
 $string = fread($input, filesize($file));
@@ -14,14 +17,34 @@ $array  = explode("\n", $string);
 fclose($input);
 
 foreach ($array as $key => $depth){
+
+    $sums[$key]     += (int)$depth ?? 0;
+    $sums[$key + 1] += (int)$depth ?? 0;
+    $sums[$key + 2] += (int)$depth ?? 0;
+    
     if ($key === 0) {
         $last_depth = $depth;
         continue;
     }
-    
+
     if ($last_depth < $depth) $measurements++;
     $last_depth = $depth;
 }
 
-echo $measurements;
+foreach ($sums as $key => $value) {
+    if($key === 0) {
+        $last_sum = $value;
+        continue;
+    }
+
+    if ($key > 2) {
+        if ($last_sum < $value) $measurements_p2++;
+        $last_sum = $value;
+    }
+}
+
+echo '<h1>Day 1</h1>';
+echo '<span>Answer Part 1 : </span>' . $measurements;
+echo '<br/>';
+echo '<span>Answer Part 2 : </span>' . $measurements_p2;
 ?>
